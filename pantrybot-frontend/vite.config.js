a552a33ce769f9369adcd330e74eaed341ecfa74
Gavin Kirwan <gavin.kirwan@icloud.com>
2024-10-25 18:21:32 -0500

    Update vite config

--- pantrybot-frontend/vite.config.js
+++ pantrybot-frontend/vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-        quietDeps: true,
-      }
-    }
-  }
+  plugins: [react()]
 })
