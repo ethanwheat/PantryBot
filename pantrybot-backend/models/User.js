52d69161952470de508a0d536ff09bf18bc0a8cd
ethanwheat <ethanwheat@ku.edu>
2024-10-25 19:48:32 -0500

    length restrictions

--- pantrybot-backend/models/User.js
+++ pantrybot-backend/models/User.js
@@ -5,7 +5,9 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        minlength: 6,
+        maxlength: 12
     },
     email: {
         type: String,
@@ -14,7 +16,8 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength: 6
     },
     oboarded: {
         type: Boolean,
