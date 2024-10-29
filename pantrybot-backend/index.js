7ededfd93aff6aa938d761af5c2b1e2055d5023a
ethanwheat <ethanwheat@ku.edu>
2024-10-29 15:33:54 -0500

    authenticateToken middleware

--- pantrybot-backend/index.js
+++ pantrybot-backend/index.js
@@ -1,4 +1,4 @@
-// backend/index 
+// pantrybot-backend/index.js 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -23,5 +23,5 @@ mongoose.connect(config.mongoURI)
 
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- pantrybot-backend/models/User.js
+++ pantrybot-backend/models/User.js
@@ -1,4 +1,4 @@
-// backend/models/User.js
+// pantrybot-backend/models/User.js
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
