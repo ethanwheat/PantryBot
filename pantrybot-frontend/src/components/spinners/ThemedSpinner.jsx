799d533b2f125b5f33400e8311e78fb4cdfa4241
Gavin Kirwan <gavin.kirwan@icloud.com>
2024-11-20 23:54:33 -0600

    Fix

--- pantrybot-frontend/src/components/spinners/ThemedSpinner.jsx
+++ pantrybot-frontend/src/components/spinners/ThemedSpinner.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Spinner } from 'react-bootstrap'
+import React from "react";
+import { Spinner } from "react-bootstrap";
 
 export default function ThemedSpinner(otherProps) {
   return (
     <Spinner animation="border" role="status" {...otherProps}>
       <span className="visually-hidden">Loading...</span>
     </Spinner>
-  )
+  );
 }
